f9b2c6348432c1b26dbf88031a9fecd9a58c0109
JoseIgnacioTapia <josignaciot@gmail.com>
2021-10-13 15:31:09 -0300

    Adding AddMovie Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -41,6 +42,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = movie => {
+    console.log(movie);
+  };
+
   let content = <p>Movie Not Found.</p>;
 
   if (movies.length > 0) {
@@ -55,6 +60,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
